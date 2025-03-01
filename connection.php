522212767a624b65a848a3774fdb0a0f2fa4c30c
Raghs3 <tinku.minku2803@gmail.com>
2025-03-01 11:49:59 +0530

    something

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+ $dbservername = "localhost";
+ $dbusername = "root";
+ $dbpassword = "root";
+ $dbname = "scheme_connect";
+
+ $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+ ?>
